3f1f73bffb906d0f95a01aa43935f344836f464a
Matt Lantz <mattlantz@gmail.com>
2021-04-15 18:32:53 -0400

    because of an accidental null forcing

--- src/Helpers/mission_control.php
+++ src/Helpers/mission_control.php
@@ -6,13 +6,13 @@ use Grafite\MissionControlLaravel\Notify;
 if (! function_exists('mission_control_issue')) {
     function mission_control_issue($message, $tag = null)
     {
-        return app(Issue::class)->log($message, $tag = null);
+        return app(Issue::class)->log($message, $tag);
     }
 }
 
 if (! function_exists('mission_control_notify')) {
     function mission_control_notify($title, $tag = null, $message = null)
     {
-        return app(Notify::class)->send($title, $tag = null, $message = null);
+        return app(Notify::class)->send($title, $tag, $message);
     }
 }
