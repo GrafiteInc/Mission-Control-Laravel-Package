a9306a2557a1bfd57c777e9f8ef50064775d0271
Matt Lantz <mattlantz@gmail.com>
2021-04-15 17:56:22 -0400

    because tags and message can be null

--- src/Helpers/mission_control.php
+++ src/Helpers/mission_control.php
@@ -4,15 +4,15 @@ use Grafite\MissionControlLaravel\Issue;
 use Grafite\MissionControlLaravel\Notify;
 
 if (! function_exists('mission_control_issue')) {
-    function mission_control_issue($message, $tag)
+    function mission_control_issue($message, $tag = null)
     {
-        return app(Issue::class)->log($message, $tag);
+        return app(Issue::class)->log($message, $tag = null);
     }
 }
 
 if (! function_exists('mission_control_notify')) {
-    function mission_control_notify($title, $tag, $message = null)
+    function mission_control_notify($title, $tag = null, $message = null)
     {
-        return app(Notify::class)->send($title, $tag, $message = null);
+        return app(Notify::class)->send($title, $tag = null, $message = null);
     }
 }
