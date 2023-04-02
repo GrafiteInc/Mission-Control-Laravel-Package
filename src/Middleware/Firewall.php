cd94d9c96c63d41d48efecbc267f295cab683895
Matt Lantz <mattlantz@gmail.com>
2023-04-02 17:24:03 -0400

    because of environments

--- src/Middleware/Firewall.php
+++ src/Middleware/Firewall.php
@@ -17,53 +17,55 @@ class Firewall
      */
     public function handle($request, Closure $next)
     {
-        $session = session();
-        $ipAddress = $request->ip();
-        $service = app(Security::class);
-        $session->put('mission-control.ip', $ipAddress);
+        if (app()->environment(config('mission-control.environments', ['production']))) {
+            $session = session();
+            $ipAddress = $request->ip();
+            $service = app(Security::class);
+            $session->put('mission-control.ip', $ipAddress);
 
-        if (! session('mission-control.bad-actor')) {
-            [$ip, $geo] = $service->lookup($ipAddress);
+            if (! session('mission-control.bad-actor')) {
+                [$ip, $geo] = $service->lookup($ipAddress);
 
-            // If their IP has changed.
-            if (session('mission-control.ip') !== $ipAddress) {
-                $session->forget('mission-control.valid-ip');
-            }
+                // If their IP has changed.
+                if (session('mission-control.ip') !== $ipAddress) {
+                    $session->forget('mission-control.valid-ip');
+                }
 
-            if (! session('mission-control.valid-ip') && $ip) {
-                // ip is whitelisted, or they are not in the blacklist
-                $session->put('mission-control.valid-ip', true);
-            }
+                if (! session('mission-control.valid-ip') && $ip) {
+                    // ip is whitelisted, or they are not in the blacklist
+                    $session->put('mission-control.valid-ip', true);
+                }
 
-            if (! session('mission-control.valid-geo') && $geo) {
-                // geo is good (not in a blocked country)
-                $session->put('mission-control.valid-geo', true);
-            }
+                if (! session('mission-control.valid-geo') && $geo) {
+                    // geo is good (not in a blocked country)
+                    $session->put('mission-control.valid-geo', true);
+                }
 
-            // If any checks fail then set as bad actor.
-            foreach (['geo', 'ip'] as $check) {
-                if (! session("mission-control.valid-{$check}")) {
-                    $threat = $service->recordThreat("invalid-{$check}", $request->input());
+                // If any checks fail then set as bad actor.
+                foreach (['geo', 'ip'] as $check) {
+                    if (! session("mission-control.valid-{$check}")) {
+                        $threat = $service->recordThreat("invalid-{$check}", $request->input());
+                        $session->put('mission-control.bad-actor', true);
+                    }
+                }
+
+                // is a malcious action
+                if ($malicious = $service->isMalicious($request)) {
+                    $threat = $malicious;
                     $session->put('mission-control.bad-actor', true);
                 }
             }
 
-            // is a malcious action
-            if ($malicious = $service->isMalicious($request)) {
-                $threat = $malicious;
-                $session->put('mission-control.bad-actor', true);
+            if (isset($threat) && session('mission-control.bad-actor')) {
+                event(new AttackDetected(array_merge($threat, ['ip' => $ipAddress])));
             }
-        }
 
-        if (isset($threat) && session('mission-control.bad-actor')) {
-            event(new AttackDetected(array_merge($threat, ['ip' => $ipAddress])));
-        }
-
-        if (! isset($threat) && session('mission-control.bad-actor')) {
-            event(new AttackDetected(array_merge([
-                'type' => 'Banned Actor',
-                'data' => $request->input()
-            ], ['ip' => $ipAddress])));
+            if (! isset($threat) && session('mission-control.bad-actor')) {
+                event(new AttackDetected(array_merge([
+                    'type' => 'Banned Actor',
+                    'data' => $request->input()
+                ], ['ip' => $ipAddress])));
+            }
         }
 
         return $next($request);
